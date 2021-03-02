af914c8d850ba5a844206f503f05e29601e5e069
MullaN <admullan@gmail.com>
2021-03-01 16:04:35 -0800

    completed limit with Mathf Clamp tutorial

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-
     [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMin = 0;
+    [SerializeField] float xMax = 14;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class Paddle : MonoBehaviour
     {
         float mouseXPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(mouseXPos, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXPos, xMin, xMax);
         transform.position = paddlePos;
     }
 }
