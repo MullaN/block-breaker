9065c3caf520541eb950185771edf53e3f537d4e
MullaN <admullan@gmail.com>
2021-03-04 12:26:39 -0800

    completed display player score tutorial

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // config params
     [Range(0.1f,10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    void Start() {
+        scoreText.text = "0";
+    }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+        scoreText.text = currentScore.ToString();
     }
 
     public void AddToScore() {
