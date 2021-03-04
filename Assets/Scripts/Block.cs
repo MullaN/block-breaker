93cffc9977fd7d11f4c7ac96ab5efaf814b38b5b
MullaN <admullan@gmail.com>
2021-03-03 16:40:33 -0800

    completed count breakable blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,14 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    //cached reference
+    Level level;
+
+    private void Start(){
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBLocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision){
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
